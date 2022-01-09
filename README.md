2c95cdf59f30a5f2cb9b5543df05a6761631e220
johnybx <johnybx@users.noreply.github.com>
2022-01-09 15:27:22 +0100

    init - updated code generated by oryx

---
+++ README.md
@@ -0,0 +1,3 @@
+## QMK keymap for ZSA moonlander mark I (base generated with Oryx)
+
+Firmware build is setup with [ZSA qmk firmware repo](https://github.com/zsa/qmk_firmware).
---
+++ rules.mk
@@ -0,0 +1,12 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+WEBUSB_ENABLE = yes
+ORYX_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+SRC = matrix.c
+UNICODE_ENABLE = yes
+LEADER_ENABLE = yes
+KEY_LOCK_ENABLE = yes
